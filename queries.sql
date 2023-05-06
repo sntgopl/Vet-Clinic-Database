a27e8fe04c3d2eda1fc20110fa504c345e3e76ff
Santiago <santi.ponsle@gmail.com>
2023-05-06 01:22:34 -0300

    Add transactions'
    q

--- queries.sql
+++ queries.sql
@@ -9,3 +9,30 @@ select * from animals where neutered = true;
 select * from animals where name != 'Gabumon';
 select * from animals where weight_kg >= 10.4 and weight_kg <=17.3;
 
+BEGIN TRANSACTION;
+UPDATE animals
+    SET species = 'unspecified';
+ROLLBACK;
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'pokemon';
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+COMMIT;
+
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+COMMIT
+
+SELECT COUNT (name) FROM animals;
+SELECT COUNT (name) FROM animals WHERE escape_attemps = 0;
+SELECT AVG (weight_kg) FROM animals;
+SELECT SUM (escape_attemps), neutered FROM animals GROUP BY neutered;
+SELECT species, MAX (weight_kg), MIN (weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG (escape_attemps) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
