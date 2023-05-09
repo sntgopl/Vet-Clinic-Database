50243156ebf869efa702126df81b75dc70b00a3c
Kola Kolade <kolakolade@gmail.com>
2023-05-09 22:50:37 +0100

    Add tables invoices & invoice_items

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,3 +10,19 @@ CREATE TABLE medical_histories (
     patient_id INT REFERENCES patients(id),
     status VARCHAR(50)
 );
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatment(id)
+);
