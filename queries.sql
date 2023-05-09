c07ef32a3e5da22819d3ac9f9f585552d0babca3
Kola Kolade <kolakolade@gmail.com>
2023-05-09 15:26:40 +0100

    Update queries.sql to query the database for optimization

--- queries.sql
+++ queries.sql
@@ -122,6 +122,7 @@ JOIN species ON animals.species_id = species.id
 WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name;
 
+
 SELECT COUNT(*) FROM visits where animal_id = 4;
 SELECT * FROM visits where vet_id = 2;
-SELECT * FROM owners where email = 'owner_18327@mail.com';
+SELECT * FROM owners where email = 'owner_18327@mail.com';'
